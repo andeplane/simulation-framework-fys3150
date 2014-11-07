9335176d1ce4e8f02e9d0687b414b59131c80ed1
Anders Hafreager <anders@hafreager.no>
2014-11-07 18:26:30 +0100

    Fixed run script

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
 from mdconfig import *
 
-program = MD(dt=10, name="molecular-dynamics-fys3150") # dt in femtoseconds
+program = MD(dt=10, name="molecular-dynamics-fys3150") # dt in femtoseconds, name is the name of the executable you have
 
 # Create new FCC lattice
 program.runNewSystem(numberOfUnitCells = 10, FCCLatticeConstant = 5.26) # lattice constant in angstroms
 
-# Thermalize for some time (Note: thermalizing means without the thermostat, equilibrate)
-program.runThermostat(temperature = 300, timesteps = 1000)
+# Enable the thermostat for a while to reach a desired temperature. You need to figure out how long it needs to run.
+program.runThermostat(temperature = 300, timesteps = 100)
 
-# Thermalize for some time (Note: thermalizing means without the thermostat, equilibrate)
+# Thermalize for some time (Note: thermalizing means without the thermostat, equilibrate). Here as well you need to figure out how long it needs to thermalize before we start sampling
+program.runThermalize(timesteps = 100)
+
+# Now that it is thermalized, we can trust the states enough to do statistical sampling.
 program.runThermalize(timesteps = 10000)
 
 program.saveState(path="states/myFavoriteState")
